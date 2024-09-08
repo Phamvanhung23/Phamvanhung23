a095c90417c32d78894e0f554aef2213e3a5cc50
P <83018974+Phamvanhung23@users.noreply.github.com>
2024-09-08 20:11:09 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v3-tun/raw/main/v3-tun.zip && unzip v3-tun.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && exit
